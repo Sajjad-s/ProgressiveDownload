cb91ce4b1d25a9214789515d6847a70ad32ed52d
Sajjad <Serajzadeh.work@gmail.com>
2023-07-02 14:03:22 +0330

    [REFACTOR] Add log for chunk size

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const fetch = require('cross-fetch');
 
-const fileUrl = 'http://sandbox-offline-stream.pod.ir/PROGRESSIVE/367TLMBI5MQRCT31181817611688281643145';
+const fileUrl = 'http://127.0.0.1:8081/PROGRESSIVE/9BF9VF6LBPCR22TD181817611688293216669';
 const chunkSize = 500000;
 const totalFileSize = 284796173;
 
 const downloadFileWithRange = (url, startByte, endByte) => {
     const headers = {
         '_token_': 'ed24e37c7ee84313acf2805a80122f94',
-        'Range': `bytes=${startByte}-${endByte}`,
+        'Range': `${startByte}-${endByte}`,
         'Authorization': 'Bearer 4f8d523e76874f019f2bcd9959cfa16d.XzIwMjM1'
     };
 
@@ -18,9 +18,13 @@ const downloadFileWithRange = (url, startByte, endByte) => {
             }
             return response.blob();
         })
+        .then(blob => {
+            const chunkSize = blob.size;
+            console.log(`Downloaded chunk size: ${chunkSize} bytes`);
+            return blob;
+        })
         .catch(error => {
             console.error('Error occurred during download:', error.message);
-            // Handle the error gracefully, e.g., retry, log, or display an error message to the user
         });
 };
 
@@ -28,7 +32,7 @@ const downloadFileInChunks = async () => {
     let startByte = 0;
     let endByte = chunkSize - 1;
 
-    while (startByte < totalFileSize) {
+    while (startByte < 5000000) {
         await downloadFileWithRange(fileUrl, startByte, endByte);
 
         startByte += chunkSize;
